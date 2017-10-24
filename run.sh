38b72dbe560d239bc3961132132ded48a552690b
Mark Lesswing <mark@lesswing.com>
2017-10-23 23:47:41 -0500

    retry failed kafka starts

--- run.sh
+++ run.sh
@@ -7,9 +7,32 @@ echo "'----------------"
 
 DIVIDER="-------------------------------"
 USER_MESSAGE=""
+OPTION_1_TRAIL=""
+OPTION_2_TRAIL=""
+OPTION_3_TRAIL="*"
+OPTION_4_TRAIL="*"
+OPTION_5_TRAIL="*"
 
 help_screen() {
-  echo -e "Invalid option.\n Choose from the list below"
+  echo -e "Invalid option.\n Choose from the list below.\n * indicates an illogical choice."
+}
+
+run_status() {
+  RUN_COUNT=0;
+  if $(/usr/bin/systemctl -q is-active couchdb) ; then
+    ((RUN_COUNT++))
+  fi
+  if $(/usr/bin/systemctl -q is-active kafka) ; then
+    ((RUN_COUNT++))
+  fi
+  if $(/usr/bin/systemctl -q is-active zookeeper) ; then
+    ((RUN_COUNT++))
+  fi
+  if [ $RUN_COUNT = '3' ]; then
+    echo true;
+  else
+    echo false;
+  fi
 }
 
 display_options() {
@@ -21,12 +44,13 @@ if ! [ "$USER_MESSAGE" = "" ]; then
   echo " $USER_MESSAGE"
   echo $DIVIDER
 fi
-echo " 1) Provision"
-echo " 2) Start Nodes"
-echo " 3) Create Channel"
-echo " 4) Install Chaincode"
-echo " 5) Stop Nodes"
+echo " 1) Provision $OPTION_1_TRAIL"
+echo " 2) Start Nodes $OPTION_2_TRAIL"
+echo " 3) Create Channel $OPTION_3_TRAIL"
+echo " 4) Install Chaincode $OPTION_4_TRAIL"
+echo " 5) Stop Nodes $OPTION_5_TRAIL"
 echo ""
+echo " ?) Help"
 echo " q) Quit"
 echo $DIVIDER
 echo ""
@@ -43,14 +67,49 @@ if [ "$USER_OPTION" = "q" -o "$USER_OPTION" = "Q" ]; then
 else
   case $USER_OPTION in
     1) ./provision.sh
+       echo $DIVIDER
+       read -p "Hit Enter to return" 
+       OPTION_1_TRAIL="*" 
+       OPTION_1_TRAIL="" 
+       OPTION_3_TRAIL="*" 
+       OPTION_4_TRAIL="*" 
+       OPTION_5_TRAIL="*" 
        USER_MESSAGE="All nodes are provisoned" ;;
-    2) ./start_nodes.sh 
+    2) ./start_nodes.sh
+       echo $DIVIDER
+       read -p "Hit Enter to return" 
+       OPTION_1_TRAIL="*" 
+       OPTION_2_TRAIL="*" 
+       OPTION_3_TRAIL="" 
+       OPTION_4_TRAIL="*" 
+       OPTION_5_TRAIL="" 
        USER_MESSAGE="Nodes have started" ;;
     3) ./create_channel.sh 
+       echo $DIVIDER
+       read -p "Hit Enter to return" 
+       OPTION_1_TRAIL="*" 
+       OPTION_2_TRAIL="*" 
+       OPTION_3_TRAIL="*" 
+       OPTION_4_TRAIL="" 
+       OPTION_5_TRAIL="" 
        USER_MESSAGE="Channel created" ;;
     4) ./install_chaincode.sh 
+       echo $DIVIDER
+       read -p "Hit Enter to return" 
+       OPTION_1_TRAIL="*" 
+       OPTION_2_TRAIL="*" 
+       OPTION_3_TRAIL="*" 
+       OPTION_4_TRAIL="*" 
+       OPTION_5_TRAIL="" 
        USER_MESSAGE="Chaincode installed" ;;
     5) ./stop_nodes.sh
+       echo $DIVIDER
+       read -p "Hit Enter to return" 
+       OPTION_1_TRAIL="" 
+       OPTION_2_TRAIL="" 
+       OPTION_3_TRAIL="*" 
+       OPTION_4_TRAIL="*" 
+       OPTION_5_TRAIL="*" 
        USER_MESSAGE="Nodes have stopped" ;;
     *) USER_MESSAGE=$(help_screen) ;;
   esac
@@ -58,6 +117,14 @@ else
 fi
 }
 
+if [ "$(run_status)" = "true" ]; then
+  USER_MESSAGE="Nodes are active"
+  OPTION_1_TRAIL="*"
+  OPTION_2_TRAIL="*"
+  OPTION_3_TRAIL="*"
+  OPTION_4_TRAIL="*"
+  OPTION_5_TRAIL=""
+fi
 display_options
 
 
--- start_nodes.sh
+++ start_nodes.sh
@@ -1,12 +1,49 @@
 
 export CONFIG_DIR=blockr_config
-export DEBUG=false
+export DEBUG=true
 export FABRIC_PATH=$GOPATH/src/github.com/hyperledger/fabric
 export FABRIC_CFG_PATH=$FABRIC_PATH/$CONFIG_DIR
+export MAX_RETRIES=7
 export ORDERER_DRIVER_NAME=start_orderer_driver.sh
 export PEER_DRIVER_NAME=start_peer_driver.sh
 export START_DAEMON_DRIVER_NAME=start_daemon_driver.sh
-export WAIT_SECONDS=0
+export START_ZOOKEEPER_DRIVER_NAME=start_zookeeper_driver.sh
+export WAIT_SECONDS=5
+
+start_zookeeper() {
+  echo "----------"
+  echo " Starting Zookeeper on Node $1"
+  echo "----------"
+
+  echo '#!/bin/bash' > $START_ZOOKEEPER_DRIVER_NAME
+  echo '' >> $START_ZOOKEEPER_DRIVER_NAME
+  echo '#----------------' >> $START_ZOOKEEPER_DRIVER_NAME
+  echo '#' >> $START_ZOOKEEPER_DRIVER_NAME
+  echo '# Block R Start Zookeeper Driver' >> $START_ZOOKEEPER_DRIVER_NAME
+  echo '#' >> $START_ZOOKEEPER_DRIVER_NAME
+  echo '#----------------' >> $START_ZOOKEEPER_DRIVER_NAME
+  echo 'echo -n " - Zookeeper "' >> $START_ZOOKEEPER_DRIVER_NAME
+  echo 'if $(/usr/bin/systemctl -q is-active zookeeper) ; then' >> $START_ZOOKEEPER_DRIVER_NAME
+  echo '  echo " already running"' >> $START_ZOOKEEPER_DRIVER_NAME
+  echo 'else' >> $START_ZOOKEEPER_DRIVER_NAME
+  echo '  echo " starting"' >> $START_ZOOKEEPER_DRIVER_NAME
+  echo '  sudo systemctl start zookeeper' >> $START_ZOOKEEPER_DRIVER_NAME
+#  if ! [ $WAIT_SECONDS = 0 ]; then
+#    echo 'echo "   - Wait for Zookeeper to start"' >> $START_ZOOKEEPER_DRIVER_NAME
+#    echo -n 'sleep ' >> $START_ZOOKEEPER_DRIVER_NAME
+#    echo $WAIT_SECONDS >> $START_ZOOKEEPER_DRIVER_NAME
+#    echo 'echo "   - Zookeeper started"' >> $START_ZOOKEEPER_DRIVER_NAME
+#  fi
+  echo 'fi' >> $START_ZOOKEEPER_DRIVER_NAME
+
+  scp -q ./$START_ZOOKEEPER_DRIVER_NAME $1: 
+  ssh $1 "chmod 777 $START_ZOOKEEPER_DRIVER_NAME"
+  ssh $1 "./$START_ZOOKEEPER_DRIVER_NAME"
+  if [ "$DEBUG" != true ]; then
+    ssh $1 "rm ./$START_ZOOKEEPER_DRIVER_NAME"
+  fi
+  rm ./$START_ZOOKEEPER_DRIVER_NAME
+}
 
 start_daemons() {
   echo "----------"
@@ -27,28 +64,40 @@ start_daemons() {
   echo '  echo "- starting"' >> $START_DAEMON_DRIVER_NAME
   echo '  sudo systemctl start couchdb' >> $START_DAEMON_DRIVER_NAME
   echo 'fi' >> $START_DAEMON_DRIVER_NAME
-
-  echo 'echo -n " - Zookeeper "' >> $START_DAEMON_DRIVER_NAME
-  echo 'if $(/usr/bin/systemctl -q is-active zookeeper) ; then' >> $START_DAEMON_DRIVER_NAME
-  echo '  echo " already running"' >> $START_DAEMON_DRIVER_NAME
-  echo 'else' >> $START_DAEMON_DRIVER_NAME
-  echo '  echo " starting"' >> $START_DAEMON_DRIVER_NAME
-  echo '  sudo systemctl start zookeeper' >> $START_DAEMON_DRIVER_NAME
-  if ! [ $WAIT_SECONDS = 0 ]; then
-    echo 'echo "   - Wait for Zookeeper to start"' >> $START_DAEMON_DRIVER_NAME
-    echo -n 'sleep ' >> $START_DAEMON_DRIVER_NAME
-    echo $WAIT_SECONDS >> $START_DAEMON_DRIVER_NAME
-    echo 'echo "   - Zookeeper started"' >> $START_DAEMON_DRIVER_NAME
-  fi
-  echo 'fi' >> $START_DAEMON_DRIVER_NAME
-
   echo 'echo -n " - Kafka "' >> $START_DAEMON_DRIVER_NAME
-  echo 'if $(/usr/bin/systemctl -q is-active kafka) ; then' >> $START_DAEMON_DRIVER_NAME
-  echo '  echo "- already running"' >> $START_DAEMON_DRIVER_NAME
-  echo 'else' >> $START_DAEMON_DRIVER_NAME
-  echo '  echo "- starting"' >> $START_DAEMON_DRIVER_NAME
-  echo '  sudo systemctl start kafka' >> $START_DAEMON_DRIVER_NAME
-  echo 'fi' >> $START_DAEMON_DRIVER_NAME
+  echo -n 'MAX_RETRIES=' >> $START_DAEMON_DRIVER_NAME
+  echo $MAX_RETRIES >> $START_DAEMON_DRIVER_NAME
+  echo 'COUNTER=1' >> $START_DAEMON_DRIVER_NAME
+  echo 'start_kafka() {' >> $START_DAEMON_DRIVER_NAME
+  echo '  if $(/usr/bin/systemctl -q is-active kafka) ; then' >> $START_DAEMON_DRIVER_NAME
+  echo '    echo "- already running"' >> $START_DAEMON_DRIVER_NAME
+  echo '  else' >> $START_DAEMON_DRIVER_NAME
+  echo '    sudo systemctl start kafka' >> $START_DAEMON_DRIVER_NAME
+  if ! [ $WAIT_SECONDS = 0 ]; then
+    echo '    echo -n "  - Wait for Kafka to start. "' >> $START_DAEMON_DRIVER_NAME
+    echo -n '    sleep ' >> $START_DAEMON_DRIVER_NAME
+    echo $WAIT_SECONDS >> $START_DAEMON_DRIVER_NAME
+  fi
+  echo '    if $(/usr/bin/systemctl -q is-active kafka) ; then' >> $START_DAEMON_DRIVER_NAME
+  echo '      COUNTER=1' >> $START_DAEMON_DRIVER_NAME
+  echo '      echo "Started."' >> $START_DAEMON_DRIVER_NAME
+  echo '    else' >> $START_DAEMON_DRIVER_NAME
+  echo '      if [ $COUNTER -le $MAX_RETRIES ] ; then' >> $START_DAEMON_DRIVER_NAME
+  echo '        ((COUNTER++))' >> $START_DAEMON_DRIVER_NAME
+  echo '        echo "Failed to start, retry."' >> $START_DAEMON_DRIVER_NAME
+  echo '        echo -n "         "' >> $START_DAEMON_DRIVER_NAME
+  echo '        start_kafka' >> $START_DAEMON_DRIVER_NAME
+  echo '      fi' >> $START_DAEMON_DRIVER_NAME
+  echo '    fi' >> $START_DAEMON_DRIVER_NAME
+  echo '  fi' >> $START_DAEMON_DRIVER_NAME
+  echo '}' >> $START_DAEMON_DRIVER_NAME
+  echo 'start_kafka' >> $START_DAEMON_DRIVER_NAME
+#  echo 'if $(/usr/bin/systemctl -q is-active kafka) ; then' >> $START_DAEMON_DRIVER_NAME
+#  echo '  echo "- already running"' >> $START_DAEMON_DRIVER_NAME
+#  echo 'else' >> $START_DAEMON_DRIVER_NAME
+#  echo '  echo "- starting"' >> $START_DAEMON_DRIVER_NAME
+#  echo '  sudo systemctl start kafka' >> $START_DAEMON_DRIVER_NAME
+#  echo 'fi' >> $START_DAEMON_DRIVER_NAME
 
   scp -q ./$START_DAEMON_DRIVER_NAME $1: 
   ssh $1 "chmod 777 $START_DAEMON_DRIVER_NAME"
@@ -137,8 +186,16 @@ echo "| Block R Provisoner"
 echo "|"
 echo "'----------------"
 
+start_zookeeper vm1
+start_zookeeper vm2
+
 start_daemons vm1
 start_daemons vm2
+
+#if ! [ $WAIT_SECONDS = 0 ]; then
+#  echo "   - PAUSE"
+#  sleep $WAIT_SECONDS
+#fi
 
 start_node_driver vm1
 start_node_driver vm2
