ee5ed361ff3492581213ad815489d06073b4a508
Mark Lesswing <mark@lesswing.com>
2017-10-27 17:05:31 -0500

    consolidates peer and orderer drivers

--- start_nodes.sh
+++ start_nodes.sh
@@ -4,9 +4,8 @@ export DEBUG=false
 export FABRIC_PATH=$GOPATH/src/github.com/hyperledger/fabric
 export FABRIC_CFG_PATH=$FABRIC_PATH/$CONFIG_DIR
 export MAX_RETRIES=7
-export ORDERER_DRIVER_NAME=start_orderer_driver.sh
-export PEER_DRIVER_NAME=start_peer_driver.sh
 export START_DAEMON_DRIVER_NAME=start_daemon_driver.sh
+export START_NODE_DRIVER_NAME=start_node_driver.sh
 export START_ZOOKEEPER_DRIVER_NAME=start_zookeeper_driver.sh
 export WAIT_SECONDS=5
 
@@ -96,73 +95,47 @@ start_node_driver() {
   echo " Start Node $1"
   echo "----------"
 
-  echo '#!/bin/bash' > $PEER_DRIVER_NAME
-  echo '' >> $PEER_DRIVER_NAME
-  echo '#----------------' >> $PEER_DRIVER_NAME
-  echo '#' >> $PEER_DRIVER_NAME
-  echo '# Block R Peer Start Driver' >> $PEER_DRIVER_NAME
-  echo '#' >> $PEER_DRIVER_NAME
-  echo '#----------------' >> $PEER_DRIVER_NAME
-  echo -n 'export FABRIC_PATH=' >> $PEER_DRIVER_NAME
-  echo $FABRIC_PATH >> $PEER_DRIVER_NAME
-  echo -n 'export FABRIC_CFG_PATH=' >> $PEER_DRIVER_NAME
-  echo $FABRIC_CFG_PATH >> $PEER_DRIVER_NAME
+  echo '#!/bin/bash' > $START_NODE_DRIVER_NAME
+  echo '' >> $START_NODE_DRIVER_NAME
+  echo '#----------------' >> $START_NODE_DRIVER_NAME
+  echo '#' >> $START_NODE_DRIVER_NAME
+  echo '# Block R Start Node Driver' >> $START_NODE_DRIVER_NAME
+  echo '#' >> $START_NODE_DRIVER_NAME
+  echo '#----------------' >> $START_NODE_DRIVER_NAME
+  echo -n 'export FABRIC_PATH=' >> $START_NODE_DRIVER_NAME
+  echo $FABRIC_PATH >> $START_NODE_DRIVER_NAME
+  echo -n 'export FABRIC_CFG_PATH=' >> $START_NODE_DRIVER_NAME
+  echo $FABRIC_CFG_PATH >> $START_NODE_DRIVER_NAME
   if [ "$DEBUG" = true ]; then
-    echo 'export CORE_LOGGING_LEVEL=debug' >> $PEER_DRIVER_NAME
+    echo 'export GENERAL_LOGLEVEL=debug' >> $START_NODE_DRIVER_NAME
+    echo 'export CORE_LOGGING_LEVEL=debug' >> $START_NODE_DRIVER_NAME
   fi
-  echo 'echo " - Peer"' >> $PEER_DRIVER_NAME
-  echo -n 'export LOG_FILE=' >> $PEER_DRIVER_NAME
-  echo -n '$FABRIC_PATH/' >> $PEER_DRIVER_NAME
-  echo -n $1 >> $PEER_DRIVER_NAME
-  echo '_peer.out' >> $PEER_DRIVER_NAME
-  echo 'if [ -f $LOG_FILE ]; then' >> $PEER_DRIVER_NAME
-  echo '  rm $LOG_FILE' >> $PEER_DRIVER_NAME
-  echo 'fi' >> $PEER_DRIVER_NAME
-  echo '$FABRIC_PATH/build/bin/peer node start &> $LOG_FILE &' >> $PEER_DRIVER_NAME
+  echo 'echo " - Peer"' >> $START_NODE_DRIVER_NAME
+  echo -n 'export LOG_FILE=' >> $START_NODE_DRIVER_NAME
+  echo -n '$FABRIC_PATH/' >> $START_NODE_DRIVER_NAME
+  echo -n $1 >> $START_NODE_DRIVER_NAME
+  echo '_peer.out' >> $START_NODE_DRIVER_NAME
+  echo 'if [ -f $LOG_FILE ]; then' >> $START_NODE_DRIVER_NAME
+  echo '  rm $LOG_FILE' >> $START_NODE_DRIVER_NAME
+  echo 'fi' >> $START_NODE_DRIVER_NAME
+  echo '$FABRIC_PATH/build/bin/peer node start &> $LOG_FILE &' >> $START_NODE_DRIVER_NAME
+  echo 'echo " - Orderer"' >> $START_NODE_DRIVER_NAME
+  echo -n 'export LOG_FILE=' >> $START_NODE_DRIVER_NAME
+  echo -n '$FABRIC_PATH/' >> $START_NODE_DRIVER_NAME
+  echo -n $1 >> $START_NODE_DRIVER_NAME
+  echo '_orderer.out' >> $START_NODE_DRIVER_NAME
+  echo 'if [ -f $LOG_FILE ]; then' >> $START_NODE_DRIVER_NAME
+  echo '  rm $LOG_FILE' >> $START_NODE_DRIVER_NAME
+  echo 'fi' >> $START_NODE_DRIVER_NAME
+  echo '$FABRIC_PATH/build/bin/orderer &> $LOG_FILE &' >> $START_NODE_DRIVER_NAME
 
-  scp -q ./$PEER_DRIVER_NAME $1:
-  ssh $1 "chmod 777 $PEER_DRIVER_NAME"
-  ssh $1 "./$PEER_DRIVER_NAME"
+  scp -q ./$START_NODE_DRIVER_NAME $1:
+  ssh $1 "chmod 777 $START_NODE_DRIVER_NAME"
+  ssh $1 "./$START_NODE_DRIVER_NAME"
   if [ "$DEBUG" != true ]; then
-    ssh $1 "rm ./$PEER_DRIVER_NAME"
+    ssh $1 "rm ./$START_NODE_DRIVER_NAME"
   fi
-  rm ./$PEER_DRIVER_NAME
-
-  echo '#!/bin/bash' > $ORDERER_DRIVER_NAME
-  echo '' >> $ORDERER_DRIVER_NAME
-  echo '#----------------' >> $ORDERER_DRIVER_NAME
-  echo '#' >> $ORDERER_DRIVER_NAME
-  echo '# Block R Orderer Start Driver' >> $ORDERER_DRIVER_NAME
-  echo '#' >> $ORDERER_DRIVER_NAME
-  echo '#----------------' >> $ORDERER_DRIVER_NAME
-  echo -n 'export FABRIC_PATH=' >> $ORDERER_DRIVER_NAME
-  echo $FABRIC_PATH >> $ORDERER_DRIVER_NAME
-  echo -n 'export FABRIC_CFG_PATH=' >> $ORDERER_DRIVER_NAME
-  echo $FABRIC_CFG_PATH >> $ORDERER_DRIVER_NAME
-  if [ "$DEBUG" = true ]; then
-    echo 'export GENERAL_LOGLEVEL=debug' >> $ORDERER_DRIVER_NAME
-  fi
-  echo 'echo " - Orderer"' >> $ORDERER_DRIVER_NAME
-  echo -n 'export LOG_FILE=' >> $ORDERER_DRIVER_NAME
-  echo -n '$FABRIC_PATH/' >> $ORDERER_DRIVER_NAME
-  echo -n $1 >> $ORDERER_DRIVER_NAME
-  echo '_orderer.out' >> $ORDERER_DRIVER_NAME
-  echo -n 'export LOG_FILE=' >> $ORDERER_DRIVER_NAME
-  echo -n '$FABRIC_PATH/' >> $ORDERER_DRIVER_NAME
-  echo -n $1 >> $ORDERER_DRIVER_NAME
-  echo '_orderer.out' >> $ORDERER_DRIVER_NAME
-  echo 'if [ -f $LOG_FILE ]; then' >> $ORDERER_DRIVER_NAME
-  echo '  rm $LOG_FILE' >> $ORDERER_DRIVER_NAME
-  echo 'fi' >> $ORDERER_DRIVER_NAME
-  echo '$FABRIC_PATH/build/bin/orderer &> $LOG_FILE &' >> $ORDERER_DRIVER_NAME
-
-  scp -q ./$ORDERER_DRIVER_NAME $1:
-  ssh $1 "chmod 777 $ORDERER_DRIVER_NAME"
-  ssh $1 "./$ORDERER_DRIVER_NAME"
-  if [ "$DEBUG" != true ]; then
-    ssh $1 "rm ./$ORDERER_DRIVER_NAME"
-  fi
-  rm ./$ORDERER_DRIVER_NAME
+  rm ./$START_NODE_DRIVER_NAME
 }
 
 echo ".----------------"
