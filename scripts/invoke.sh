e4da2e780fb1de5b0256b4a2d451fd96c1366cdc
Mark Lesswing <mark@lesswing.com>
2017-10-28 11:44:55 -0500

    filter out informatory messages and check for errors

--- scripts/invoke.sh
+++ scripts/invoke.sh
@@ -9,5 +9,15 @@ if [ "$WITH_TLS" = true ]; then
   ORDERER_TLS="--tls --cafile $FABRIC_CFG_PATH/ordererOrganizations/nar.blockr/orderers/vm1.nar.blockr/tls/ca.crt"
 fi
 
-$FABRIC_PATH/build/bin/peer chaincode invoke -n exampleCC -v 1.0 -C blockr -c '{"Args":["invoke","a","b","10"]}' -o vm1:7050 $ORDERER_TLS
+$FABRIC_PATH/build/bin/peer chaincode invoke -n exampleCC -v 1.0 -C blockr -c '{"Args":["invoke","a","b","10"]}' -o vm1:7050 $ORDERER_TLS &> ./result.txt
+while read line ; do
+  if [[ $line == *"result: status:"* ]]; then
+    echo ${line#*result:}
+  else
+    if [[ $line == *"Error:"* ]]; then
+      echo ${line#*Error:}
+    fi
+  fi
+done < ./result.txt
+rm result.txt
 
--- scripts/query.sh
+++ scripts/query.sh
@@ -9,5 +9,15 @@ if [ "$WITH_TLS" = true ]; then
   ORDERER_TLS="--tls --cafile $FABRIC_CFG_PATH/ordererOrganizations/nar.blockr/orderers/vm1.nar.blockr/tls/ca.crt"
 fi
 
-$FABRIC_PATH/build/bin/peer chaincode query -n exampleCC -v 1.0 -C blockr -c '{"Args":["query","a"]}' -o vm1:7050 $ORDERER_TLS 
+$FABRIC_PATH/build/bin/peer chaincode query -n exampleCC -v 1.0 -C blockr -c '{"Args":["query","a"]}' -o vm1:7050 $ORDERER_TLS &> ./result.txt
+while read line ; do
+  if [[ $line == *"Query Result:"* ]]; then
+echo "$line"
+  else
+    if [[ $line == *"Error:"* ]]; then
+      echo ${line#*Error:}
+    fi
+  fi
+done < ./result.txt 
+rm result.txt
 
