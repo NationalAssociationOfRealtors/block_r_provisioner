53f0df7a2073b5118b3fcd610083966c609d33c4
Mark Lesswing <mark@lesswing.com>
2017-11-13 21:51:41 -0600

    added documentation describint zookeeper versus node configuration

--- config.sh
+++ config.sh
@@ -1,5 +1,18 @@
+################################################################################
+#
+#  Block R Network Configuration
+#
+################################################################################
+
+#
+# Zookeeper configurationam - There should be two servers configured.
+#
 zookeepers="vm1 vm2"
 zookeeper_accounts="mal mal"
+
+#
+# Node configuration - There should be an odd number of servers
+#
 nodes="vm1 vm2"
 accounts="mal mal"
 domains="nar.blockr car.blockr"
@@ -7,3 +20,4 @@ orderer_names="NAR-Orderer-0 CAR-Orderer-0"
 orderers="Orderer1MSP Orderer2MSP"
 peer_names="NAR-Peer-0 CAR-Peer-0"
 peers="Org1MSP Org2MSP"
+
